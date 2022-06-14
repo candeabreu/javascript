764515c2b1e73138cd5b7a325a48a76219ecd705
CandelariaAbreu <candelariaabreu.18@gmail.com>
2022-06-14 20:16:15 -0300

    declaracionesyasignaciones

---
+++ javascript/mainindex.js
@@ -0,0 +1,38 @@
+//declaracion de variables
+let nombre;
+let apellido;
+let nombreCompleto;
+
+/* 
+//asignacion de valores
+
+nombre = "Candelaria";
+apellido = "Abreu";
+nombreCompleto = nombre + "" + apellido;
+
+//declarar variable e inicializarla a la vez en una sola linea
+let numero = 8;
+let numero2 = 21;
+let resultado = numero + numero2; //resultado seria igual a 29
+
+//constantes con mayuscula, se declaran y se inician
+const PI= 3.141516;
+
+*/
+ 
+let nombre2 = prompt ("ingrese su nombre");
+let apellido2 = prompt ("ingresar su apellido");
+let numero1 = 8;
+let numero2 = 21;
+let resultado = numero1 + numero2;
+
+
+//console=salida de datos
+
+console.log (nombre2)
+console.log (apellido2)
+console.log ("el resultado de la sema es:" + resultado)
+
+
+
+
